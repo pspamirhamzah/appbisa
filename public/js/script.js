9abea2c984c5b1a0f2757f05fa6fb31654c151af
Amir Hamzah <pspamirhamzah@gmail.com>
2025-12-24 13:02:35 +0700

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -329,7 +329,7 @@ const app = (() => {
 
         try {
             // URL MENGGUNAKAN 'v1beta' (SESUAI SCREENSHOT ANDA)
-            const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.0-pro:generateContent?key=${apiKey}`, {
+            const response = await fetch(`https://generativelanguage.googleapis.com/v1/models/gemini-1.5-pro-latest:generateContent?key=${apiKey}`, {
 
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
